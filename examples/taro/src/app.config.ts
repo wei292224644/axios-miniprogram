41ee2ed41917ed42697893e2dedcfb4803ba516d
wwj <wei292224644@live.com>
2024-09-20 15:46:52 +0800

    update

--- examples/taro/src/app.config.ts
+++
@@ -1,9 +0,0 @@
-export default defineAppConfig({
-  pages: ['home'],
-  window: {
-    backgroundTextStyle: 'light',
-    navigationBarBackgroundColor: '#fff',
-    navigationBarTitleText: 'WeChat',
-    navigationBarTextStyle: 'black',
-  },
-});
